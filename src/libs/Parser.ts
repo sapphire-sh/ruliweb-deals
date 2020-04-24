acc09dc5718f5b85aa12226ed67ff9332403f3f8
sapphire <info@sapphire.sh>
2020-04-24 17:48:05 +0900

    fix type

--- src/libs/Parser.ts
+++ src/libs/Parser.ts
@@ -24,7 +24,7 @@ export class Parser {
 			}
 			const type = $(column[1]).text().trim();
 			const title = $(column[2]).find('a').text().trim();
-			const link = $(column[2]).find('a').attr('href');
+			const link = $(column[2]).find('a').attr('href') ?? '';
 			const tweet = -1;
 
 			return { id, type, title, link, tweet };
