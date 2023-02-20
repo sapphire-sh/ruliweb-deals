2c9b13ab69b835aa7059340ccc11837c447bab67
sapphire <info@sapphire.sh>
2023-02-21 05:15:58 +0900

    remove redis

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,7 @@
 import { sleep } from '@sapphire-sh/utils';
-import IORedis from 'ioredis';
 import schedule from 'node-schedule';
 import { Database, Parser, Tweeter } from '~/libs';
+import { Item } from './models';
 
 export class App {
 	private readonly database: Database;
@@ -9,41 +9,41 @@ export class App {
 	private readonly tweeter: Tweeter;
 
 	public constructor() {
-		const redis = new IORedis({ host: process.env.REDIS_HOST });
-
-		this.database = new Database(redis);
+		this.database = new Database();
 		this.parser = new Parser();
 		this.tweeter = new Tweeter(__config);
 	}
 
-	private async parse(): Promise<void> {
-		const items = await this.parser.parse();
-		for (const item of items) {
-			await this.database.insertItem(item);
+	private async parse(): Promise<Item[]> {
+		return await this.parser.parse();
+	}
+
+	private async tweet(item: Item): Promise<void> {
+		try {
+			if (item.id <= this.database.lastId) {
+				return;
+			}
+
+			await this.tweeter.tweetItem(item);
+			item.tweet = 1;
+			this.database.update(item);
+			await sleep(1000);
+		} catch (error) {
+			console.log(error);
 		}
 	}
 
-	private async tweet(): Promise<void> {
-		const items = await this.database.getUntweetedItems();
+	private async process() {
+		const date = new Date();
+		console.log('parse', date);
+
+		const items = await this.parse();
 		for (const item of items) {
-			try {
-				await this.tweeter.tweetItem(item);
-				item.tweet = 1;
-				await this.database.updateItem(item);
-				await sleep(1000);
-			} catch (error) {
-				console.log(error);
-			}
+			await this.tweet(item);
 		}
 	}
 
 	public async start() {
-		schedule.scheduleJob('*/2 * * * *', async () => {
-			const date = new Date();
-			console.log('parse', date);
-
-			await this.parse();
-			await this.tweet();
-		});
+		schedule.scheduleJob('*/2 * * * *', () => this.process());
 	}
 }
--- src/libs/__tests__/Database.test.ts
+++ src/libs/__tests__/Database.test.ts
@@ -1,11 +1,10 @@
 import { faker } from '@faker-js/faker';
-import IORedis from 'ioredis-mock';
 import { Item } from '~/models';
 import { Database } from '../Database';
 
-const getRandomItem = (defaultId: number): Item => {
+const getRandomItem = (defaultId?: number): Item => {
 	return {
-		id: faker.datatype.number() + defaultId + 1,
+		id: faker.datatype.number() + (defaultId ?? 0) + 1,
 		type: faker.random.alphaNumeric(8),
 		title: faker.random.alphaNumeric(8),
 		link: faker.random.alphaNumeric(8),
@@ -17,118 +16,15 @@ describe('libs/Database', () => {
 	let database: Database;
 	let prevItem: Item;
 
-	beforeAll(() => {
-		const redis = new IORedis();
-		database = new Database(redis);
-	});
-
 	beforeEach(async () => {
-		prevItem = getRandomItem(database.defaultId);
-
-		await database.flush();
-		await database.insertItem(prevItem);
+		database = new Database();
+		prevItem = getRandomItem(database.lastId);
 	});
 
-	describe('getItem', () => {
+	describe('update', () => {
 		test('success', async () => {
-			const item = await database.getItem(prevItem.id);
-			expect(item).not.toBeNull();
-		});
-
-		test('failure - not found', async () => {
-			const id = faker.datatype.number();
-			const item = await database.getItem(id);
-			expect(item).toBeNull();
-		});
-	});
-
-	describe('getItems', () => {
-		test('success', async () => {
-			const items = await database.getItems();
-			expect(items).toHaveLength(1);
-			expect(items).toContainEqual(prevItem);
-		});
-	});
-
-	describe('getUntweetedItems', () => {
-		test('success', async () => {
-			const items = await database.getUntweetedItems();
-			expect(items).toHaveLength(1);
-			expect(items[0]).toEqual(prevItem);
-		});
-
-		test('success - no items', async () => {
-			prevItem.tweet = 1;
-
-			const res = await database.updateItem(prevItem);
-			expect(res).toBe(true);
-
-			const items = await database.getUntweetedItems();
-			expect(items).toHaveLength(0);
-		});
-	});
-
-	describe('insertItem', () => {
-		test('success', async () => {
-			const item = getRandomItem(database.defaultId);
-
-			const res = await database.insertItem(item);
-			expect(res).toBe(true);
-
-			const nextItem = await database.getItem(item.id);
-			expect(nextItem).toEqual(item);
-		});
-
-		test('failure - duplicate', async () => {
-			const res = await database.insertItem(prevItem);
-			expect(res).toBe(false);
-
-			const nextItem = await database.getItem(prevItem.id);
-			expect(nextItem).toEqual(prevItem);
-		});
-	});
-
-	describe('updateItem', () => {
-		test('success', async () => {
-			prevItem.tweet = 1;
-
-			const res = await database.updateItem(prevItem);
-			expect(res).toBe(true);
-
-			const nextItem = await database.getItem(prevItem.id);
-			expect(nextItem).not.toBeNull();
-			expect(nextItem!.tweet).toBe(1);
-		});
-
-		test('failure', async () => {
-			prevItem.tweet = 1;
-
-			{
-				const res = await database.updateItem(prevItem);
-				expect(res).toBe(true);
-			}
-
-			prevItem.tweet = 0;
-			const res = await database.updateItem(prevItem);
-			expect(res).toBe(false);
-
-			const nextItem = await database.getItem(prevItem.id);
-			expect(nextItem).not.toBeNull();
-			expect(nextItem!.tweet).toBe(1);
-		});
-	});
-
-	describe('getLastID', () => {
-		test('success', async () => {
-			const id = await database.getLastId();
-			expect(id).toBe(prevItem.id);
-		});
-
-		test('success - default id', async () => {
-			await database.flush();
-
-			const id = await database.getLastId();
-			expect(id).toBe(database.defaultId);
+			database.update(prevItem);
+			expect(database.lastId).toBe(prevItem.id);
 		});
 	});
 });
